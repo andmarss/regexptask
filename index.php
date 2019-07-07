324aea3358dc34c13a5bac4ba30b05e04d1f4d19
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-07-07 11:53:48 +0300

    task

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @param string $text
+ * @return array
+ */
+function getPasswordSumAndWallet(string $text): array {
+    // пароль
+    preg_match('/(\d{4})/', $text, $password);
+
+    // сколько спишется
+    preg_match('/(\d+[\,|\.]\d+\s?р\.?)/', $text, $sum);
+
+    // номер кошелька
+    preg_match('/(\d{15})/', $text, $wallet);
+
+    return [
+        isset($password[0]) ? current($password) : null,
+        isset($sum[0]) ? current($sum) : null,
+        isset($wallet[0]) ? current($wallet) : null
+    ];
+}
+
+$text = "Пароль: 9159
+Спишется 111,56р.
+Перевод на счет 410012156350833";
+
+[$password, $sum, $wallet] = getPasswordSumAndWallet($text);
+
+var_dump($password, $sum, $wallet);
